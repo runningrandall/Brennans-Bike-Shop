5210d3b3c1baf76cea4f1f6c69d6af2557166b57
Randall Adams <rjadams96@gmail.com>
2022-11-14 19:45:46 -0700

    change content for testing

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -20,12 +20,11 @@ export default function Index() {
             <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
                 <span className="block uppercase text-red-500 drop-shadow-md">
-                  Grunge Stack
+                  Brennan's Bike Shop
                 </span>
               </h1>
               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
-                Check the README.md file for instructions on how to get this
-                project deployed.
+                This is gonna be the best bike shop eva!!
               </p>
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
