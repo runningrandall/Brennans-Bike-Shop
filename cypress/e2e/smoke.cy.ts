4b2930ecbb4276ab7e91b24c9b5d0907dd3524c1
Randall Adams <rjadams96@gmail.com>
2022-11-15 18:47:39 -0700

    simplify tests

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,26 +1,13 @@
 import { faker } from "@faker-js/faker";
 
 describe("smoke tests", () => {
-  it("should allow you to register and login", () => {
-    const loginForm = {
-      email: `${faker.internet.userName()}@example.com`,
-      password: faker.internet.password(),
-    };
-    cy.then(() => ({ email: loginForm.email })).as("user");
+  it("should rint the title", () => {
 
     cy.visitAndCheck("/");
-    cy.findByRole("link", { name: /sign up/i }).click();
-
-    cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
-    cy.findByLabelText(/password/i).type(loginForm.password);
-    cy.findByRole("button", { name: /create account/i }).click();
-
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByRole("button", { name: /logout/i }).click();
-    cy.findByRole("link", { name: /log in/i });
+    cy.get('h1').should('exist');
   });
 
-  it("should allow you to make a note", () => {
+  it.skip("should allow you to make a note", () => {
     const testNote = {
       title: faker.lorem.words(1),
       body: faker.lorem.sentences(1),
